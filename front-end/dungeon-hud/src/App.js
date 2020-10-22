e1aaac7a9323aae94b8a24963c5e0797910b7497
mattkerkstra <mtk2@rice.edu>
2020-10-22 17:50:51 -0500

    add typescript

--- front-end/dungeon-hud/src/App.js
+++ front-end/dungeon-hud/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import characterSheet from './characterSheet/CharacterSheet';
 
 function App() {
   return (
     <div className="App">
+      <characterSheet />
+      {() => characterSheet}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ front-end/dungeon-hud/src/characterSheet/CharacterSheet.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './CharacterSheet.css';
+
+const characterSheet = props => (
+    <div>
+        remember
+
+        remember
+
+        remember
+
+        remember
+
+        remember
+
+        remember
+
+        remember
+
+        remember
+
+        remember
+
+        re
+
+        mem
+
+        ber
+
+        locally? not really. 
+
+        nope..
+
+        I've got part of my burger project but a lot of it lives on my workstation.. and I'm not logging back into the VPN
+
+        we are - haven't set that bit up.
+
+        a very sad one - in the intitial create-react-app project.
+    </div>
+);
+
+export default characterSheet;
+
