747c22bc9313ff6d59fb65fc3cace396108d9653
williamzmacdonald <33135172+williamzmacdonald@users.noreply.github.com>
2020-11-01 17:19:21 -0600

    convert to typescript, make test names component

---
+++ front-end/dungeon-hud/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CharacterSheet from './components/characterSheet/CharacterSheet';
+
+function App() {
+  return (
+    <div className="App">
+      <CharacterSheet />
+    </div>
+  );
+}
+
+export default App;
