4c4f1ce0507552ce142f383172c93502d69d905e
mkerkstra <mattkerkstra@gmail.com>
2020-11-19 18:11:13 -0600

    .

--- dungeon-hud-api/src/character-sheet/models/character-sheet.model.ts
+++ dungeon-hud-api/src/character-sheet/models/character-sheet.model.ts
@@ -1,5 +1,7 @@
 import mongoose, { Schema, Document } from 'mongoose';
-import { IAbilityScores } from './ability-scores.model';
+import { IAbilityScores, AbilityScoresSchema } from './ability-scores.model';
+import { composeMongoose } from 'graphql-compose-mongoose';
+import { schemaComposer } from 'graphql-compose';
 
 export interface ICharacterSheet extends Document {
     readonly name: string;
@@ -7,10 +9,33 @@ export interface ICharacterSheet extends Document {
     readonly description?: string;
 }
 
-const CharacterSheetSchema: Schema = new Schema({
-    name: { type: String, required: true },
-    abilityScores: { type: Schema.Types.ObjectId, required: true },
-    description: { type: String, required: false }
-});
+// STEP 1: DEFINE MONGOOSE SCHEMA AND MODEL
+const AbilityScoresSchema = new mongoose.Schema({
+    strength: { type: Number, required: true },
+    dexterity: { type: Number, required: true },
+    constitution: { type: Number, required: true },
+    intelligence: { type: Number, required: true },
+    wisdom: { type: Number, required: true },
+    charisma: { type: Number, required: true },
+})
+const AbilityScores = mongoose.model('AbilityScores', AbilityScoresSchema);
 
-export default mongoose.model<ICharacterSheet>('CharacterSheet', CharacterSheetSchema);
+const CharacterSheetSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    abilityScores: {
+        type: [AbilityScoresSchema],
+        default: [],
+        alias: 'abilityScores',
+    },
+    description: { type: String, required: false },
+});
+const CharacterSheet = mongoose.model('CharacterSheet', CharacterSheetSchema);
+
+// STEP 2: CONVERT MONGOOSE MODEL TO GraphQL PIECES
+const customizationOptions = {};
+const CharacterSheetTC = composeMongoose(CharacterSheet, customizationOptions);
+
+// STEP 3: Add needed CRUD User operations to the GraphQL Schema
+schemaComposer.Query.addFields({
+    CharacterSheetTC.mongooseResolvers.findAll(),
+})
