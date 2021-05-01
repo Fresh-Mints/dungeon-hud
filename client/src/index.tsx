e843b83c62b040c466cba7413c68812a4701fead
mkerkstra <mattkerkstra@gmail.com>
2021-05-01 15:18:46 -0500

    http & ws auth header, add .env

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,35 +2,61 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
 import { BrowserRouter } from 'react-router-dom';
-
 import { cache } from './store/cache';
-import { ApolloClient, ApolloProvider, createHttpLink, NormalizedCacheObject } from '@apollo/client';
+import { ApolloClient, ApolloProvider, createHttpLink, NormalizedCacheObject, split } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { loader } from 'graphql.macro';
+import { config } from 'dotenv';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+config();
 
 const schema = loader('../../api/src/schema.gql')
 
 // Initialize http Link
 const httpLink = createHttpLink({
-  uri: 'http://127.0.0.1:4000/graphql',
+  uri: process.env.API_HTTP_URI ?? 'http://127.0.0.1:4000/graphql'
 });
-
-// Initialize auth link - think we can use sentry here?
+// http auth link
 const authLink = setContext((_, { headers}) => {
-    const token = localStorage.getItem('token');
+  const token = localStorage.getItem('token');
 
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? `Bearer ${token}` : '',
-        },
-    };
+  return {
+      headers: {
+          ...headers,
+          authorization: token ? `Bearer ${token}` : '',
+      },
+  };
 });
+// websocket + auth link
+const wsLink = new WebSocketLink({
+  uri: process.env.API_WS_URI ?? 'ws://http://127.0.0.1:4000/subscriptions',
+  options: {
+    reconnect: true,
+    connectionParams: {
+      isWebSocket: true,
+      headers: {
+        authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    }
+  }
+})
+// split for subscription & http
+const splitLink = split(({ query }) => {
+  const definition = getMainDefinition(query);
+  return (
+    definition.kind === 'OperationDefinition' &&
+    definition.operation === 'subscription'
+  );
+},
+wsLink,
+authLink.concat(httpLink)
+)
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: splitLink,
   cache: cache,
   typeDefs: schema,
 })
